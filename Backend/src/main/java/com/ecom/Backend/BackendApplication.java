d9239ccdd4ce3557bdc43efd959a62c9772d5428
Promiles0 <apaersod@gmail.com>
2026-08-11 16:45:24 +0200

    Resolved merge conflicts

--- Backend/src/main/java/com/ecom/Backend/BackendApplication.java
+++ Backend/src/main/java/com/ecom/Backend/BackendApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import jakarta.annotation.PostConstruct;
 
 @EnableAsync
 @SpringBootApplication
 public class BackendApplication {
+
+	@Value("${DB_URL:NOT_SET}")
+	private String debugDbUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -17,5 +22,10 @@ public class BackendApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@PostConstruct
+	public void logDbUrl() {
+		System.out.println("DB_URL as seen by Spring: " + debugDbUrl);
 	}
 }
